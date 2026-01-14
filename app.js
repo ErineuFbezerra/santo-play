08958481c512237dbd20ffaf6e8eef70d49b2f5c
ErineuFbezerra <erineufbezerra@gmail.com>
2026-01-14 19:24:19 -0300

    Refactor playLouvor function to single line

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 function playLouvor() {
-  window.open(
-    "https://www.youtube.com/watch?v=ZkVZ9n7h7Ew",
-    "_blank"
-  );
+  window.open("https://www.youtube.com/watch?v=ZkVZ9n7h7Ew", "_blank");
 }
-
