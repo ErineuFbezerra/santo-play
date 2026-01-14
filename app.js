cf746722a56a542c93d0d30f37bf6d25497474df
ErineuFbezerra <erineufbezerra@gmail.com>
2026-01-14 19:35:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 function playLouvor() {
-  window.open("https://www.youtube.com/watch?v=ZkVZ9n7h7Ew", "_blank");
+  window.open(
+    "https://www.youtube.com/watch?v=ZkVZ9n7h7Ew",
+    "_blank"
+  );
 }
